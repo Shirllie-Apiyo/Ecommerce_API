704e0fa02953668cbde2a0f973dbb938421e3fff
Shirllie-Apiyo <apraise320@gmail.com>
2022-01-24 18:59:23 +0300

    connect to db and

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("hello")
+const express = require ("express");
+const app = express();
+const mongoose = require("mongoose");
+
+
+mongoose
+    .connect("mongodb+srv://customer:customer@project.wsekr.mongodb.net/shop?retryWrites=true&w=majority"
+    ).then(() =>  console.log("Db Connection Successful!"))
+    .catch((err) =>{
+        console.log(err);
+});
+app.listen(5000, () =>{
+    console.log("Server Running at http://127.0.0.1:5000");
+});
