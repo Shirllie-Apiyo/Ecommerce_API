af3d580b1fd7acfcce358aa501191d1968872926
Shirllie-Apiyo <apraise320@gmail.com>
2022-01-25 23:17:30 +0300

    cors

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require ("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors")
+
 
 dotenv.config();
 
@@ -11,6 +13,7 @@ mongoose
         .then(() =>  console.log("Db Connection Successful!"))
     .catch((err) =>{
         console.log(err);
+        
 });
 app.listen(process.env.PORT || 5000, () =>{
     console.log("Server Running at http://127.0.0.1:5000");
