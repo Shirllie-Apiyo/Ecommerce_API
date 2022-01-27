13b6ef2765a3cb781f63120e59cc39591f34cff0
Shirllie-Apiyo <apraise320@gmail.com>
2022-01-27 12:55:43 +0300

    rest api

--- README.md
+++ README.md
@@ -11,4 +11,6 @@ Ecommerce backend in Nodejs
 9. to run :nodemon app.js
 10. configure nodemon in package.json by erasing the test and type "start": "nodemon app.js"
 11. to start nodemon type npm start on the terminal
-12 go to cloud.mongodb and create your account and cluster and connect to your db
+12. go to cloud.mongodb and create your account and cluster and connect to your db
+13. To secure your database name and password create an env file and in that file create a url and link the env file with the app.js file.
+14. create a REST api. To test it run localhost:5000/api/test if that is your end point and it will reload on the terminal.
--- app.js
+++ app.js
@@ -2,19 +2,22 @@ const express = require ("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const cors = require("cors")
+// const cors = require("cors")
 
 
 dotenv.config();
 
 mongoose
-    .connect
-        (process.env.MONGO_URL)
-        .then(() =>  console.log("Db Connection Successful!"))
+    .connect(process.env.MONGO_URL)
+    .then(() =>  console.log("Db Connection Successful!"))
     .catch((err) =>{
-        console.log(err);
-        
+        console.log(err);        
 });
+
+app.get("/api/test", ()=>{
+    console.log("test is successful");
+});
+
 app.listen(process.env.PORT || 5000, () =>{
     console.log("Server Running at http://127.0.0.1:5000");
 });
