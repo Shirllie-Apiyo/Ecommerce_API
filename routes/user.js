b61f04645d540157d31a3490efcadccd031a1807
Shirllie-Apiyo <apraise320@gmail.com>
2022-01-29 23:53:10 +0300

    post route

--- routes/user.js
+++ routes/user.js
@@ -4,4 +4,13 @@ router.get("/usertest", (req,res) =>{
     res.send("user test is successful");
 });
 
+//localhost:5000/api/user/usertest
+
+router.post("userposttest",(req,res)=>{
+    const username = req.body.username;
+    console.log(username);
+});
+
+
+
 module.exports = router;
