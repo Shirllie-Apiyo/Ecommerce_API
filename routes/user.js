d2b145b6fc496d9088e2f97e63fdd079e335e685
Shirllie-Apiyo <apraise320@gmail.com>
2022-02-01 00:52:08 +0300

    register&login routes and password hashing

--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,4 @@
 const router = require ("express").Router();
 
-router.get("/usertest", (req,res) =>{
-    res.send("user test is successful");
-});
-
-//localhost:5000/api/user/usertest
-
-router.post("/userposttest",(req,res)=>{
-    const username = req.body.username;
-    res.send("your username is" +username);
-});
-
-
 
 module.exports = router;
