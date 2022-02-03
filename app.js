4c8840c0e458ba2c1d320197f1bab83116b15584
Shirllie-Apiyo <apraise320@gmail.com>
2022-02-03 23:06:12 +0300

    order route

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
-// const orderRoute = require("./routes/order");
+const orderRoute = require("./routes/order");
 // const stripeRoute = require("./routes/stripe");
 const cors = require("cors");
 
@@ -26,7 +26,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
-// app.use("/api/orders", orderRoute);
+app.use("/api/orders", orderRoute);
 // app.use("/api/checkout", stripeRoute);
 
 
